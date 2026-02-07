aedb3ab1015ee9d2bfeaecdd9c98147bd1ee8c52
Manas Salimzhan <m.salimzhan@jusan.kz>
2026-02-07 12:33:55 +0500

    Initial Commit

---
+++ SmartFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartFit
+//
+//  Created by Manas Salimzhan on 07.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
